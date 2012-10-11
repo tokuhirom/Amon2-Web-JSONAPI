5cec6b23288ab70e696f45b976c833f871a8057a
tokuhirom <tokuhirom@gmail.com>
2012-10-12 05:29:29 +0900

    initial import

---
+++ lib/Amon2/Web/JSONAPI/DFV.pm
@@ -0,0 +1,39 @@
+package Amon2::Web::JSONAPI::DFV;
+use strict;
+use warnings;
+use 5.010001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Amon2::Web::JSONAPI::DFV - A module for you
+
+=head1 SYNOPSIS
+
+  use Amon2::Web::JSONAPI::DFV;
+
+=head1 DESCRIPTION
+
+Amon2::Web::JSONAPI::DFV is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
